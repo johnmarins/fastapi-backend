5fa018bd5c8637b9d25c1ddd08a0278ebd8eb41f
jmarinsocha <jmarinsocha@gmail.com>
2024-07-06 15:11:23 +0000

    Commit inicial

---
+++ backend/type_hints.py
@@ -0,0 +1,12 @@
+my_string_variable = "My String Variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: int = 5
+my_string_variable
+print(my_typed_variable)
+print(type(my_typed_variable))
