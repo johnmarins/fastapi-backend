d538a1abbac61db376f02bfb4cf6c8c083a84ab5
jmarinsocha <jmarinsocha@gmail.com>
2024-07-15 17:04:27 +0000

    Path y Query

--- backend/fastapi/users.py
+++ backend/fastapi/users.py
@@ -5,20 +5,42 @@ app = FastAPI()
 
 #Entidad User
 class User(BaseModel):
+    id: int
     name: str
     lastname: str
     url: str
     age: int
 
 
-users_list = [User(name="John", lastname="Marin", url="https://www.google.com", age=25),
-            User(name="Ana", lastname="Perez", url="https://www.facebook.com", age=30),
-            User(name="Juan", lastname="Sanchez", url="https://www.twitter.com", age=40)]
+users_list = [User(id=1, name="John", lastname="Marin", url="https://www.google.com", age=25),
+            User(id=2, name="Ana", lastname="Perez", url="https://www.facebook.com", age=30),
+            User(id=3, name="Juan", lastname="Sanchez", url="https://www.twitter.com", age=40)]
 
 
 @app.get("/usersjson")
 async def usersJson():
     return users_list
+
+#Path
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except IndexError:
+        return {"error": "User not found"}
+
+#Query
+@app.get("/userquery/")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except IndexError:
+        return {"error": "User not found"}
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
